45258a3a4b8cc8c1f7e93b0fec36bbab456cfa2e
abgrunddd <878581145@qq.com>
2019-04-16 21:43:00 +0800

    up

--- Weixian Ye DAT505-Assignment/js/index.js
+++ Weixian Ye DAT505-Assignment/js/index.js
@@ -3,28 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 // Frame rate statistics
 var stats;
+
 function initStats() {
   stats = new Stats();
   document.getElementById('canvas-frame').appendChild(stats.dom);
 }
 var sphere = new THREE.SphereBufferGeometry( 0.5, 16, 8 );
-//lights
-var light1, light2, light3, light4,
-light1 = new THREE.PointLight( 0xff0040, 2, 50 );
-light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
-scene.add( light1 );
 
-light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
-light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
-scene.add( light2 );
-
-light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
-light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
-scene.add( light3 );
-
-light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
-light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
-scene.add( light4 );
+var clock = new THREE.Clock();
 
 //create a renderer
 var renderer;
@@ -50,6 +36,28 @@ function initCamera() {
   camera.position.z = -500;
   camera.lookAt({ x: 0, y: 0, z: 0 });
 }
+
+function render() {
+  var time = Date.now() * 0.0005;
+  var delta = clock.getDelta();
+
+  if ( object ) object.rotation.y -= 0.5 * delta;
+  light1.position.x = Math.sin( time * 0.7 ) * 30;
+  light1.position.y = Math.cos( time * 0.5 ) * 40;
+  light1.position.z = Math.cos( time * 0.3 ) * 30;
+
+  light2.position.x = Math.cos( time * 0.3 ) * 30;
+  light2.position.y = Math.sin( time * 0.5 ) * 40;
+  light2.position.z = Math.sin( time * 0.7 ) * 30;
+
+  light3.position.x = Math.sin( time * 0.7 ) * 30;
+  light3.position.y = Math.cos( time * 0.3 ) * 40;
+  light3.position.z = Math.sin( time * 0.5 ) * 30;
+
+  light4.position.x = Math.sin( time * 0.3 ) * 30;
+  light4.position.y = Math.cos( time * 0.7 ) * 40;
+  light4.position.z = Math.sin( time * 0.5 ) * 30;
+
 
 // create a sence
 var scene;
