8d20dc47ab28d22ea26b34ba670320e9604e8c0a
abgrunddd <878581145@qq.com>
2019-04-19 10:15:39 +0800

    up

--- Weixian Ye DAT505-Assignment/js/index.js
+++ Weixian Ye DAT505-Assignment/js/index.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 //
-var stats;
+/*var stats;
 function initStats() {
   stats = new Stats();
   document.getElementById('canvas-frame').appendChild(stats.dom);
-}
+}*/
 
 // create a renderer
 var renderer;
@@ -32,8 +32,8 @@ function initCamera() {
   // 透视相机 视角越大，看到的场景越大，那么中间的物体相对于整个场景来说，就越小了
   camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
   camera.position.x = -500;
-  camera.position.y = 500;
-  camera.position.z = -500;
+  camera.position.y = 800;
+  camera.position.z = 100;
   camera.lookAt({ x: 0, y: 0, z: 0 });
 }
 
@@ -43,6 +43,9 @@ function initScene() {
   scene = new THREE.Scene();
 }
 
+
+var light1, light2, light3, light4;
+var clock = new THREE.Clock();
 // create a light
 //var light;
 function initLight() {
@@ -52,9 +55,28 @@ light = new THREE.AmbientLight(0xFFFFFF);
 light.position.set(100, 100, 200);
 scene.add(light);
 
+var sphere = new THREE.SphereBufferGeometry( 16.5, 116,108 );
+
+light1 = new THREE.PointLight( 0xff0040, 1000, 100 );
+light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
+
+scene.add(light1);
+
+light2 = new THREE.PointLight( 0x0040ff, 1000, 100 );
+light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
+scene.add( light2 );
+
+light3 = new THREE.PointLight( 0x80ff80, 1000, 100 );
+light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
+scene.add( light3 );
+
+light4 = new THREE.PointLight( 0xffaa00, 1000, 100 );
+light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
+scene.add( light4 );
+
+
 }
-var light1, light2, light3, light4;
-var clock = new THREE.Clock();
+
 
 
 // create a earth
@@ -70,22 +92,6 @@ function initEarth() {
 }
 
 //lights
-
-light1 = new THREE.PointLight( 0xff0040, 2, 50 );
-light1.add( new THREE.Mesh( earthMesh, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
-scene.add( light1 );
-
-light2 = new THREE.PointLight( 0x0040ff, 2, 50 );
-light2.add( new THREE.Mesh( earthMesh, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
-scene.add( light2 );
-
-light3 = new THREE.PointLight( 0x80ff80, 2, 50 );
-light3.add( new THREE.Mesh( earthMesh, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
-scene.add( light3 );
-
-light4 = new THREE.PointLight( 0xffaa00, 2, 50 );
-light4.add( new THREE.Mesh( earthMesh, new THREE.MeshBasicMaterial( { color: 0xffaa00 } ) ) );
-scene.add( light4 );
 
 
 // create clouds
@@ -106,13 +112,13 @@ function initClouds() {
 //create stars
 var starmesh;
 function initStar(){
-  var starGeometry = new THREE.SphereGeometry(201.5, 100, 100);
+  var starGeo = new THREE.SphereGeometry(201.5, 100, 100);
   var starMater = new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load('star.jpg'),
     transparent: true,
     opacity: 0.6
   });
-  starMesh = new THREE.Mesh(starGeometry, starMater);
+  starMesh = new THREE.Mesh(starGeo, starMater);
   scene.add(starMesh);
 }
 
@@ -120,7 +126,7 @@ function initStar(){
 var controls;
 function threeStart() {
   initThree();
-  initStats();
+  //initStats();
   initCamera();
   initScene();
   initLight();
@@ -137,32 +143,20 @@ function render() {
 var time = Date.now() * 0.0005;
 
 var delta = clock.getDelta();
-
+console.log("1");
 if ( earthMesh) earthMesh.rotation.y -= 0.5 * delta;
 
-light1.position.x = Math.sin( time * 0.7 ) * 30;
-light1.position.y = Math.cos( time * 0.5 ) * 40;
-light1.position.z = Math.cos( time * 0.3 ) * 30;
 
-light2.position.x = Math.cos( time * 0.3 ) * 30;
-light2.position.y = Math.sin( time * 0.5 ) * 40;
-light2.position.z = Math.sin( time * 0.7 ) * 30;
-
-light3.position.x = Math.sin( time * 0.7 ) * 30;
-light3.position.y = Math.cos( time * 0.3 ) * 40;
-light3.position.z = Math.sin( time * 0.5 ) * 30;
-
-light4.position.x = Math.sin( time * 0.3 ) * 30;
-light4.position.y = Math.cos( time * 0.7 ) * 40;
-light4.position.z = Math.sin( time * 0.5 ) * 30;
 }
 
 function animate() {
+  var time = Date.now() * 0.0005;
 
+  var delta = clock.getDelta();
   requestAnimationFrame(animate);
 
   controls.update();
-  stats.update();
+  //stats.update();
 
   earthMesh.rotation.y += 0.004;
   cloudsMesh.rotation.y -= 0.005;
@@ -170,6 +164,25 @@ function animate() {
   starMesh.rotation.y += 0.004;
 
 
+  light1.position.x = Math.sin( time * 0.7 ) * 400;
+  light1.position.y = Math.cos( time * 0.5 ) * 400;
+  light1.position.z = Math.cos( time * 0.3 ) * 400;
+
+  light2.position.x = Math.cos( time * 0.3 ) * 400;
+  light2.position.y = Math.sin( time * 0.5 ) * 400;
+  light2.position.z = Math.sin( time * 0.7 ) * 400;
+
+  light3.position.x = Math.sin( time * 0.7 ) * 400;
+  light3.position.y = Math.cos( time * 0.3 ) * 400;
+  light3.position.z = Math.sin( time * 0.5 ) * 400;
+
+  light4.position.x = Math.sin( time * 0.3 ) * 400;
+  light4.position.y = Math.cos( time * 0.7 ) * 400;
+  light4.position.z = Math.sin( time * 0.5 ) * 400;
+
+
+  scene.add(light3);
+  scene.add(light4);
 
   renderer.render(scene, camera);
 }
